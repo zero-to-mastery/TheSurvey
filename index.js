c9b026d7434c56c65637b2d019252fdb4cbdcec1
sdeagh <huw.burton@btinternet.com>
2018-05-02 23:24:32 +0100

    adding boilerplate node/express code

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the index route. Enjoy your stay!')
+})
+
 app.listen(3000, () => {
     console.log("listening on port 3000");
 })
