8d1780f422fe3fefa18d5420901ad81777220f6c
jef88 <frost.joshe@gmail.com>
2018-05-03 06:22:39 -0400

    setup basic db config file and establish basic mongoose model and relationships

---
+++ app/model/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const Schema = mongoose.Schema;
+
+let userSchema = Schema({
+	name: String,
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		trim: true,
+		validate: {
+			validator: validator.isEmail,
+			message: '{VALUE} is not a valid email.'
+		}
+	},
+	password: {type: String, require: true, minlength: 6},
+	tokens: [{
+		access: {type: String, required: true},
+		token: {type: String, required: true}
+	}],
+	surveys: [{type: Schema.Types.ObjectId, ref: 'Survey'}]
+});
+
+let User = mongoose.model('User', userSchema);
+module.exports = {User};
