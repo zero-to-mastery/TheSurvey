fdfd735f687149db9a3f46c36391f7d29baa7eb2
Diane Leigh <leighd2008@gmail.com>
2018-05-05 06:58:32 -0400

    fixed timing of testcases, removed all but one disconect cmnds

--- app/model/user.spec.js
+++ app/model/user.spec.js
@@ -4,34 +4,38 @@ const assert = require("assert");
 const chai = require("chai");
 const user = require("./User");
 
-describe("user schema test", () => {
+describe("user schema test", function() {
     //return;
-    user.User.deleteMany({}, function(err){
-        console.log(err);
-    })
-
-    it ("should be ok", () => {
+    it ("should be ok", function(done1) {
         chai.expect(true).equals(true);
+        user.User.deleteMany({}, function(err){
+            console.log(err);
+            done1()
+        })
+
     });
-    it ("add user with a name to db and read it back", ()=>{
+    it ("add user with a name to db and read it back", function(done2) {
         chai.expect(true).equals(true);
         let myUser = new user.User({name: "Pavel", email:"addr@mail.com", password: "password123"});
-        let saveRes = myUser.save((err, theUser) => {
+        let saveRes = myUser.save(function(err, theUser) {
             if (err){ console.error(err);
-                mongoose.disconnect();
                 return;
             }
             console.log(`${theUser.name} save finished`);
-            user.User.find({name: /^Pav/}, (err, users) => {
+            user.User.find({name: /^Pav/}, function(err, users) {
                 if (err){
                     console.error(err);
-                    mongoose.disconnect();
                     return;
                 }
                 console.log(users);
-                mongoose.disconnect();
+                done2();
             })
+
         });
-        console.log(saveRes)
+        after(function(done2){
+          console.log(saveRes)
+          mongoose.disconnect(done2);
+        });
     });
+
 });
