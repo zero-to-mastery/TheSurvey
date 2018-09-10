d2b55cc8238468dbcbe9f2d811b34ba77df2bfb1
nitudeka <nitumonideka327@gmail.com>
2018-09-10 21:41:36 +0530

    added some base styles

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import UserAccount from '../pages/UserAccount/UserAccount';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <UserAccount />
       </div>
     );
   }
---
+++ src/pages/UserAccount/UserAccount.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './UserAccount.css';
+
+const UserAccount = () => {
+  return (
+    <div className='user-account'>
+      <h1>My Account</h1>
+    </div>
+  )
+}
+
+export default UserAccount;
