7a7926176b8fdf1bbfd8a3ae2d6efa02cb2fd3e5
F1r3byte <f1r3byte@yandex.com>
2018-09-12 11:29:11 +0200

    reset react to the way it should be

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import { AllSurveys } from './AllSurveys/AllSurveys';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AllSurveys />
       </div>
     );
   }
