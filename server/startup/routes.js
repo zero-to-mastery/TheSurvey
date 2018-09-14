131ad017f05ac8a6419d145d05d08bf12b7b3ba1
ayabongaqwabi <ayabonga@projectcodex.co>
2018-09-14 21:26:55 +0200

    Chore: Update: Use body parser

--- server/startup/routes.js
+++ server/startup/routes.js
@@ -10,16 +10,25 @@ const user = require('../routes/api/user')
 const auth = require('../routes/api/auth')
 
 const surveys = require('../routes/api/surveys')
+const answer = require('../routes/api/answer')
 
 module.exports = function(app) {
 	//Handlebars Middleware
 	app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 	app.set('view engine', 'handlebars')
-
+	
+	// parse application/x-www-form-urlencoded
+	app.use(bodyParser.urlencoded({ extended: false }))
+	// parse application/json
+	app.use(bodyParser.json())
+	
+	app.use('/answer', answer)
 	app.use('/api/user', user)
 
 	//Use surveys Router - this comes after middlewares
 	app.use('/surveys', surveys)
+
+
 
 	app.get('/', (req, res) => {
 		const title = 'Hi Mom!'
