04de026f663a0275471cea070562729e19ba4151
Thorben L <turbzcoding@gmail.com>
2018-09-14 21:17:00 +0200

    Adding some space for nicer overview, put proper verb on one of the routes

--- server/routes/api/surveys.js
+++ server/routes/api/surveys.js
@@ -1,17 +1,20 @@
+const SurveyController = require('../../controllers/surveyController');
 const express = require('express');
 const router = express.Router();
-const SurveyController = require('../../controllers/surveyController')
 
 // Route for testing purposes - display survey form
 router.get('/add', (req, res) => {
     res.render('surveys/add');
 });
 
- // /api/surveys is default, all routes in this file start with this path
 router.get('/', SurveyController.getAllSurveys);
+
 router.get('/:surveyID', SurveyController.getSurvey);
+
 router.post('/', SurveyController.postSurvey);
-router.patch('/:surveyID', SurveyController.updateSurvey);
+
+router.put('/:surveyID', SurveyController.updateSurvey);
+
 router.delete('/:surveyID', SurveyController.deleteSurvey);
 
 module.exports = router;
