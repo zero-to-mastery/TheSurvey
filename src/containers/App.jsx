336fd416e8edef349995f7284f43bb8d2c5ee63f
Raj Khare <seven.triangles@icloud.com>
2018-09-20 20:06:29 +0530

    Add Footer component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from '../components/Navbar/Navbar';
+import Footer from '../components/Footer/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Footer />
       </div>
     );
   }
