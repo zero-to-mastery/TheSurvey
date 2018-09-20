97008db97b05fc5d059dc2a3f0537d5ca359c12a
Raj Khare <seven.triangles@icloud.com>
2018-09-20 20:31:13 +0530

    Add SetNewPassword component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navbar from '../components/Navbar/Navbar';
-import Footer from '../components/Footer/Footer';
+import SetNewPassword from '../components/SetNewPassword/SetNewPassword';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-        <Footer />
+        <SetNewPassword />
       </div>
     );
   }
