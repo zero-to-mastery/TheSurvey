566ec81c3041de0495ca61e1a23dd3b2bb3293e2
Raj Khare <seven.triangles@icloud.com>
2018-09-20 21:13:28 +0530

    Add NewPasswordSet component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import SetNewPassword from '../components/SetNewPassword/SetNewPassword';
+import NewPasswordSet from '../components/NewPasswordSet/NewPasswordSet';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SetNewPassword />
+        <NewPasswordSet />
       </div>
     );
   }
