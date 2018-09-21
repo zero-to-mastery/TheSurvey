5ad83d70ccf1cc1fe0fc953cc99662bb75b70206
Raj Khare <seven.triangles@icloud.com>
2018-09-21 09:39:59 +0530

    Add App.css and remove unnecessary imports

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
-import NewPasswordSet from '../components/NewPasswordSet/NewPasswordSet';
-import SetNewPassword from '../components/SetNewPassword/SetNewPassword';
-import Navbar from '../components/Navbar/Navbar';
-import Footer from '../components/Footer/Footer';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-        <NewPasswordSet />
-        <SetNewPassword />
-        <Footer />
       </div>
     );
   }
